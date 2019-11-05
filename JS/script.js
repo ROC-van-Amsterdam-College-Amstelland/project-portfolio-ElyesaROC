f39b12cbec1799f87edfc69bed8cab8717ef5601
Elyesa_Caglar <elyesa.caglar2002@gmail.com>
2019-11-05 12:18:24 +0100

    de Javascript is af

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+    function verander1(){
+    document.getElementById("Img2").style.display="none";
+    document.getElementById("Img1").style.display="block";
+}
+    function verander2(){
+    document.getElementById("Img3").style.display="none";
+    document.getElementById("Img2").style.display="block";
+    }
+    function verander3(){
+        document.getElementById("Img1").style.display="none";
+        document.getElementById("Img3").style.display="block";
+    }
